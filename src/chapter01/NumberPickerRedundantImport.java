6aece07cef189ae1560b9842211c28565eba4df4
FatmanaK <janalikbolsin@gmail.com>
2024-07-29 15:57:30 -0500

    imports

---
+++ src/chapter01/NumberPickerRedundantImport.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+import java.lang.System; // Redundant Import
+import java.lang.*; // Redundant Import
+import java.util.Random;
+import java.util.*; // Redundant Import
+
+public class NumberPickerRedundantImport {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+    }
+}
